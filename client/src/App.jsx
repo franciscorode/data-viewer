fcbd988d3c0ee43447d777b5fe1a379daffff3d3
franciscorode <fran_rode@hotmail.com>
2024-12-15 11:42:41 +0100

    refactor: auto import chartjs components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,7 @@ import './App.css'
 
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip, 
-  Legend 
-);
+import 'chart.js/auto';
 
 function App() {
   const [events, setEvents] = useState([]);
